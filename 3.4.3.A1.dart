c3afec0b0bf4a53cfc7f174027fdd7015759caee
Grace <grace6424@icloud.com>
2025-03-04 13:38:33 +0100

    cleanup

--- 3.4.3.A1.dart
+++ 3.4.3.A1.dart
@@ -1,18 +1,17 @@
 void main () {
 
-  //Liste hat garantiert immer 3 Elemente
   List<Map<String, double?>> weatherData = [
     {'temp' : 5.3, 'rain' : 0.9, 'wind' : null},
     {'temp' : 4.5, 'rain' : null, 'wind' : 16.8},
     {'temp' : null, 'rain' : 3.8, 'wind' : null},
   ];
 
-  //Erstelle leere Liste für Temperaturen
+
   List<double> temps = [];
   List<double> rain = [];
   List<double> wind = [];
 
-  //Fuge die Temperaturen der Wetterdaten in die Liste ein
+
   temps.add(weatherData[0]['temp'] ?? 0);
   temps.add(weatherData[1]['temp'] ?? 0);
   temps.add(weatherData[2]['temp'] ?? 0);
@@ -25,7 +24,7 @@ void main () {
   wind.add(weatherData[1]['wind'] ?? 0);
   wind.add(weatherData[2]['wind'] ?? 0);
 
-  //Berechne den durchschnitt der Temperaturen
+
   double avgTemp = (temps[0] + temps[1] + temps[2]) / weatherData.length;
   String tempsString = avgTemp.toStringAsFixed(2);
   double avgTempRndUp = double.parse(tempsString);
